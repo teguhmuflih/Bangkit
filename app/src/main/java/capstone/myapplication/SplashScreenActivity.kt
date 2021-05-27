f01e7d294669ffa7f27e15b0a41c33fd09628a81
teguhmuflih <33649540+teguhmuflih@users.noreply.github.com>
2021-05-27 09:58:08 +0700

    menambahkan SplashScreenActivity.kt

---
+++ app/src/main/java/capstone/myapplication/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package capstone.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@Suppress("DEPRECATION")
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val splashScreenTime: Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, splashScreenTime)
+
+    }
+}

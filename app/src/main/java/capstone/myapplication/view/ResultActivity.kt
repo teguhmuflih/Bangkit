19cf25ef21a0ee4847d85d82603a11c2ec889850
diahd <diahdwiyanti93@gmail.com>
2021-06-02 14:57:15 +0700

    model ML

--- app/src/main/java/capstone/myapplication/view/ResultActivity.kt
+++ app/src/main/java/capstone/myapplication/view/ResultActivity.kt
@@ -4,14 +4,11 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import capstone.myapplication.R
-import capstone.myapplication.data.DataDummy
-import capstone.myapplication.data.DataEntity
 import capstone.myapplication.databinding.ActivityResultBinding
 import capstone.myapplication.databinding.ItemResultBinding
 import capstone.myapplication.tflite.Classifier
+import java.lang.Exception
+import java.sql.Connection
 
 class ResultActivity : AppCompatActivity() {
 
@@ -27,11 +24,8 @@ class ResultActivity : AppCompatActivity() {
         const val EXTRA_DETAIL = "extra_detail"
     }
 
-    //var photoResult: RecyclerView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_result)
 
         binding = ActivityResultBinding.inflate(layoutInflater)
         itemBinding = binding.itemResult
@@ -42,41 +36,20 @@ class ResultActivity : AppCompatActivity() {
 
         classifier = Classifier(assets, MODEL_PATH, LABEL_PATH, INPUT_SIZE)
 
-        //val data = DataDummy.generateDummyPest()
-        //val dataintent = intent.getByteArrayExtra(EXTRA_DETAIL)
         val dataIntent = Uri.parse(intent.getStringExtra(EXTRA_DETAIL))
-        //val bmp: Bitmap = BitmapFactory.decodeByteArray(dataintent, 0, dataintent!!.size)
-
-        //val degrees = 90f //rotation degree
-
-        //val matrix = Matrix()
-        //matrix.setRotate(degrees)
-        //val bOutput = Bitmap.createBitmap(bmp, 0, 0, bmp.width, bmp.height, matrix, true)
-
-        //binding.viewResult.setImageBitmap(bmp)
-        //binding.viewResult.setImageURI(dataIntent)
         val bitmapPhoto = MediaStore.Images.Media.getBitmap(this.contentResolver, dataIntent)
 
         itemBinding.viewResult.setImageBitmap(bitmapPhoto)
 
         val result = classifier.recognizeImage(bitmapPhoto)
 
-        runOnUiThread {
-            itemBinding.textViewResult.text = result.get(0).title
-        }
+        val titleResult = result.get(0).title
 
-        //binding.viewResult.setImageBitmap(bitmapPhoto)
-        //setData(data)
+        runOnUiThread {
+            itemBinding.textViewResult.text = titleResult
+        }
     }
 
-    /*private fun setData(data: List<DataEntity>) {
-        binding.rvResult.layoutManager = LinearLayoutManager(this)
-        val photoResult = findViewById<RecyclerView>(R.id.rv_result)
-        //val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
-        //photoResult.layoutManager = LinearLayoutManager(this)
-        mainResultAdapter  = MainResult(this, data)
-        photoResult.adapter = mainResultAdapter
-    }*/
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
